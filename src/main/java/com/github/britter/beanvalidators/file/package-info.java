e846a5e990ea6edaa2df97e79c99f28246895d7f
Benedikt Ritter <beneritter@gmail.com>
2015-08-02 14:59:09 +0200

    Fix checkstyle issue: Packages should have a package-info.java file

---
+++ src/main/java/com/github/britter/beanvalidators/file/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2015 Benedikt Ritter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Constraints for validating all properties of {@link java.io.File} objects.
+ *
+ * <p>
+ * There is an annotation for each property as well as one for the negation of each property.
+ * All annotations can also be applied to Strings. The validators will construct a new File instance using
+ * {@link java.io.File#File(java.lang.String)} and use that instance for validation.
+ * </p>
+ */
+package com.github.britter.beanvalidators.file;
