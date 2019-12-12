280cc7a860a28e62262a788021a6e075cc8dffb6
Benedikt Ritter <beneritter@gmail.com>
2019-12-12 17:49:44 +0100

    Add necessary configuration for compiling and running tests

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2019 Benedikt Ritter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.nio.charset.StandardCharsets
+
+description = "Additional validator implementations for javax.validation"
+
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = StandardCharsets.UTF_8.name()
+}
+
+dependencies {
+    api("javax.validation:validation-api:1.1.0.Final")
+    implementation("org.apache.commons:commons-lang3:3.6")
+    implementation("commons-validator:commons-validator:1.6")
+
+    testImplementation("junit:junit:4.12")
+    testImplementation("org.assertj:assertj-core:3.8.0")
+    testImplementation("com.google.guava:guava:23.0")
+    testImplementation("org.hibernate:hibernate-validator:5.1.3.Final")
+    testImplementation("javax.el:javax.el-api:3.0.0")
+}
