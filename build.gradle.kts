061eeac6a467616d22ceecd987f6607d56b2c1d6
Benedikt Ritter <beneritter@gmail.com>
2019-12-18 13:05:11 +0100

    Update version to next release version

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@
  */
 description = "Additional validator implementations for javax.validation"
 group = "com.github.britter"
-version = "0.6.4-SNAPSHOT"
+version = "0.7.0"
 
 plugins {
     `java-conventions`
