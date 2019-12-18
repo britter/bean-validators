27714fb0694f5e1f08aa5e6f4e3b05573e2d5107
Benedikt Ritter <beneritter@gmail.com>
2019-12-18 12:50:46 +0100

    Generated by gradle-git-publish.

---
+++ apidocs/test-fixtures/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.github.britter.beanvalidators","l":"BeanValidatorsAsserts"},{"p":"com.github.britter.beanvalidators","l":"ConstraintViolationAssert"},{"p":"com.github.britter.beanvalidators","l":"ValidationWrapper"}]
