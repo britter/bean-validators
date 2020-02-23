f1d6e012b8c37e7fa9e3126c8eeb37202f3f61d0
Benedikt Ritter <beneritter@gmail.com>
2020-02-23 17:46:45 +0100

    Use latest and greated JUnit 5 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,8 @@ dependencies {
     implementation("org.apache.commons:commons-lang3:3.6")
     implementation("commons-validator:commons-validator:1.6")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
     testRuntimeOnly("org.hibernate:hibernate-validator:5.1.3.Final")
 
     testFixturesApi("org.assertj:assertj-core:3.8.0")
