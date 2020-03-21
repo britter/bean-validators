44df1e3dc70c98ed6c66eca3f94bcea2f5b4a8a9
Benedikt Ritter <beneritter@gmail.com>
2020-03-21 18:42:58 +0100

    Activate build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
 rootProject.name = "bean-validators"
+
+plugins {
+    id("com.gradle.enterprise") version "3.1.1"
+}
+
+gradleEnterprise {
+    buildScan {
+        if(!System.getenv("CI").isNullOrEmpty() || file("gradle/.build-scans-tos-agree").exists()) {
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+            publishAlways()
+        }
+    }
+}
