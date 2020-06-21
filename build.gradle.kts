fac649ce4e42c6a5935130a2b84f4187d6d6c465
Benedikt Ritter <beneritter@gmail.com>
2020-06-21 12:54:17 +0200

    Bump version number to next development version

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@
  */
 description = "Additional validator implementations for javax.validation"
 group = "com.github.britter"
-version = "0.8.0"
+version = "0.8.1-SNAPSHOT"
 
 plugins {
     `java-conventions`
