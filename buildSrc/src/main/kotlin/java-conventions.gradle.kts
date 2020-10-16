68fda7c63a742afd60c4773dbba3feb571e32bf2
Benedikt Ritter <beneritter@gmail.com>
2020-10-16 13:57:44 +0200

    Use toolchain to set required Java version

--- buildSrc/src/main/kotlin/java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/java-conventions.gradle.kts
@@ -21,8 +21,9 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
     withJavadocJar()
     withSourcesJar()
 }
